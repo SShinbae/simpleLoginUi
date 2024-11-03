39c6e20a0d2c681154b7c63dec95d13ef5f3bac2
SShinbae <wanahnaf08@gmail.com>
2024-11-04 00:41:11 +0800

    add multiple override function in ActivityLogin

--- app/src/main/java/com/example/viewsanduielement/ActivityLogin.java
+++ app/src/main/java/com/example/viewsanduielement/ActivityLogin.java
@@ -8,6 +8,36 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ActivityLogin extends AppCompatActivity {
 
     @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
