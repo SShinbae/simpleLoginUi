0f70ab36d43e3a336639aea150634996d07cbccb
SShinbae <wanahnaf08@gmail.com>
2024-12-11 12:08:55 +0800

    update gradle

--- app/src/main/java/com/example/viewsanduielement/SecondActivityCam.java
+++ app/src/main/java/com/example/viewsanduielement/SecondActivityCam.java
@@ -9,6 +9,7 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -29,39 +30,47 @@ public class SecondActivityCam extends Activity {
     Bitmap bitmap;
     ActivityGetImgBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityGetImgBinding.inflate(getLayoutInflater());
+
         setContentView(R.layout.activity_get_img);
 
         executor = Executors.newSingleThreadExecutor();
         handler = new Handler(Looper.getMainLooper());
 
-        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 
-        if (networkInfo != null && networkInfo.isConnected()) {
-            executor.execute(() -> {
-                try {
-                    URL ImageURL = new URL("https://ftmk.utem.edu.my/web/wp-content/uploads/2020/02/cropped-Logo-FTMK.png");
-                    HttpURLConnection connection = (HttpURLConnection) ImageURL.openConnection();
-                    connection.setDoInput(true);
-                    connection.connect();
-                    InputStream inputStream = connection.getInputStream();
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inPreferredConfig = Bitmap.Config.RGB_565;
-                    bitmap = BitmapFactory.decodeStream(inputStream, null, options);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
-                handler.post(() -> binding.imgVwSelfie.setImageBitmap(bitmap));
-            });
-        } else {
-            Toast.makeText(this, "No Network!! Please add data plan or connect to wifi network!", Toast.LENGTH_SHORT).show();
-        }
+        binding.btnAsyncTask.setOnClickListener(view -> {
+            ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isConnected()) {
+                executor.execute(() -> {
+                    try {
+                        URL ImageURL = new URL("https://ftmk.utem.edu.my/web/wp-content/uploads/2020/02/cropped-Logo-FTMK.png");
+                        HttpURLConnection connection = (HttpURLConnection) ImageURL.openConnection();
+                        connection.setDoInput(true);
+                        connection.connect();
+                        InputStream inputStream = connection.getInputStream();
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inPreferredConfig = Bitmap.Config.RGB_565;
+                        bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    handler.post(() -> binding.imgVwSelfie.setImageBitmap(bitmap));
+                });
+            } else {
+                Toast.makeText(this, "No Network!! Please add data plan or connect to wifi network!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
