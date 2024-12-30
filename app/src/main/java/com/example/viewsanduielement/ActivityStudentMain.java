841fc92aee6a777cc975ad0f85c6f5cf40aee670
SShinbae <wanahnaf08@gmail.com>
2024-12-30 22:22:36 +0800

    begin work on Lab 10 works. improvise ActivityStudentMain

--- app/src/main/java/com/example/viewsanduielement/ActivityStudentMain.java
+++ app/src/main/java/com/example/viewsanduielement/ActivityStudentMain.java
@@ -7,16 +7,30 @@ import android.util.Log;
 import android.view.View;
 
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.viewsanduielement.databinding.ActivityStudentMainBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 
 public class ActivityStudentMain extends AppCompatActivity {
@@ -35,7 +49,7 @@ public class ActivityStudentMain extends AppCompatActivity {
         setContentView(binding.getRoot());
         super.onCreate(savedInstanceState);
 
-        binding.fabAdd.setOnClickListener(this:: fnAdd);
+        binding.fabAdd.setOnClickListener(this:: fnAddToRest);
         binding.strBirthdate.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -65,6 +79,59 @@ public class ActivityStudentMain extends AppCompatActivity {
         binding.rcvStud.setAdapter(adapter);
         binding.rcvStud.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    private void fnAddToRest(View view) {
+        String strURL = "http://192.168.0.117/RESTAPI/rest_api.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, strURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        })
+        {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                String fullname = binding.strFullname.getText().toString();
+                String studNo = binding.strStudNo.getText().toString();
+                String email = binding.strEmail.getText().toString();
+                String birth = binding.strBirthdate.getText().toString();
+                String gender = "";
+                String state = binding.spnState.getSelectedItem().toString();
+
+                if(binding.rbMale.isChecked()){
+                    gender = binding.rbMale.getText().toString();
+                }
+                else if (binding.rbFemale.isChecked()) {
+                    gender = binding.rbFemale.getText().toString();
+                }
+
+                Map<String,String> params = new HashMap<>();
+                params.put("selectFn", "fnSaveData");
+                params.put("fullname", fullname);
+                params.put("studNo", studNo);
+                params.put("email", email);
+                params.put("gender", gender);
+                params.put("birthdate", birth);
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
     private void fnAdd(View view) {
